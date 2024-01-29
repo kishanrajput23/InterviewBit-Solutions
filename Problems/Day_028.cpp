996043417ebd9a0648e394cef56b08596b1d5c6d
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2024-01-29 11:16:46 +0530

    Salutes

--- Problems/Day_028.cpp
+++ Problems/Day_028.cpp
@@ -1,7 +1,7 @@
 long Solution::countSalutes(string A) {
     long salute = 0;
     int ltr = 0;
-    for(int i = 0 ; i < A.size() ; i++) {
+    for (int i = 0 ; i < A.size() ; i++) {
         if (A[i] == '>') {
             ltr++;
         }
