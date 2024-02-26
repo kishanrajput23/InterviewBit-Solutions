f62f4dffe76fd05adad3de864e5d74e0cfed10f2
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2024-02-26 11:15:54 +0530

    Roman To Integer

---
+++ Problems/Day_056.cpp
@@ -0,0 +1,22 @@
+int Solution::romanToInt(string A) {
+    unordered_map<char, int> m;    
+    m['I'] = 1;
+    m['V'] = 5;
+    m['X'] = 10;
+    m['L'] = 50;
+    m['C'] = 100;
+    m['D'] = 500;
+    m['M'] = 1000;
+        
+    int ans = 0;
+        
+    for(int i = 0; i < A.length(); i++){
+        if(m[A[i]] < m[A[i+1]]){
+            ans -= m[A[i]];
+        }
+        else{
+            ans += m[A[i]];
+        }
+    }
+    return ans;    
+}
